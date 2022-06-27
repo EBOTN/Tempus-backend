a4cf6a17b97b74327514f2e1813d4a5f0c297333
EBOTN <EBOTN@mail.ru>
2022-06-28 00:42:18 +0700

    add some validation

--- src/models/create-user-dto.ts
+++ src/models/create-user-dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsEmail, IsString } from "class-validator";
+import {
+  IsNotEmpty,
+  IsEmail,
+  IsString,
+  Matches,
+  Length,
+} from "class-validator";
 
 export class CreateUserDto {
   @ApiProperty({ example: "test@test.ru", description: "Email" })
@@ -9,15 +15,19 @@ export class CreateUserDto {
   @ApiProperty({ example: "Kevin", description: "User firstname" })
   @IsString()
   @IsNotEmpty()
+  @Length(3, 15)
   readonly firstName: string;
 
   @ApiProperty({ example: "Klein", description: "User lastname" })
   @IsString()
   @IsNotEmpty()
+  @Length(3, 15)
   readonly lastName: string;
 
   @ApiProperty({ example: "123153", description: "User password" })
   @IsString()
   @IsNotEmpty()
+  @Matches(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])/, {message: "Invalid password"})
+  @Length(3, 15)
   readonly password: string;
 }
