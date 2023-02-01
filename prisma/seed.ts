0469443c270bbdedfd0189b9e50720798a0e99ab
EBOTN <EBOTN@mail.ru>
2023-02-01 12:23:41 +0700

    add prisma seed(create 1 user and 100 workspaces)

---
+++ prisma/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcryptjs";
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = "123456";
+  const email = "bebrin@mail.ru";
+  const firstName = "Mike";
+  const lastName = "Vazovsky";
+  const hashPassword = await bcrypt.hash(password, 5);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      firstName,
+      lastName,
+      password: hashPassword,
+    },
+  });
+
+  const genWorkspaces = []
+  for(let i = 0; i < 100; i++){
+    genWorkspaces.push({title: `Workspace ${i}`, ownerId: user.id})
+  }
+  const workspaces = await prisma.workSpace.createMany({
+    data: genWorkspaces
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
