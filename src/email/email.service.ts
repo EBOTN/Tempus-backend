4bce960eeae7a834d24a61b612487bb6623a323d
Andromeda <andrei.froschgaizer@yandex.ru>
2023-03-06 17:37:34 +0700

    add subject for mail

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -9,8 +9,10 @@ export class EmailService {
     const url = `http://localhost:5173/recovery-password?token=${token}`;
     return await this.mailerService.sendMail({
       to: email,
+      subject: 'Tempus password reset request',
       template: "./changePasswordTemplate",
       context: {
+        title: 'test',
         siteUrl: "http://localhost:5173",
         firstName,
         url,
