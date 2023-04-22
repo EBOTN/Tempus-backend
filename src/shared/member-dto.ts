affcb2a7f4f2e8bb605a160956aa59d518e92441
Andromeda <andrei.froschgaizer@yandex.ru>
2023-04-22 14:53:00 +0700

    add roles type for frontend types generation

--- src/shared/member-dto.ts
+++ src/shared/member-dto.ts
@@ -1,9 +1,8 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Roles } from "@prisma/client";
-import { UserDto } from "src/user/dto/user-dto";
-
+import { ApiProperty } from '@nestjs/swagger';
+import { Roles } from '@prisma/client';
+import { UserDto } from 'src/user/dto/user-dto';
 
 export class MemberDto extends UserDto {
-    @ApiProperty({description: "Member role", enum: Roles})
-    readonly role: Roles
+  @ApiProperty({ description: 'Member role', enum: Roles, enumName: 'Roles' })
+  readonly role: Roles;
 }
