031a6d323eaffbd849b4fb1980af7178e3cf94d5
EBOTN <EBOTN@mail.ru>
2023-04-25 00:23:42 +0700

    change swagger naming for "Roles"

--- src/shared/member-dto.ts
+++ src/shared/member-dto.ts
@@ -3,6 +3,6 @@ import { Roles } from '@prisma/client';
 import { UserDto } from 'src/user/dto/user-dto';
 
 export class MemberDto extends UserDto {
-  @ApiProperty({ description: 'Member role', enum: Roles, enumName: 'Roles' })
+  @ApiProperty({ description: 'Member role', enum: Roles, enumName: 'Role' })
   readonly role: Roles;
 }
