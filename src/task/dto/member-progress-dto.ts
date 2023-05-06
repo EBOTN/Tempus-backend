75ba73b107ef7ea0cc7e36aa1974db2a6e16e601
EBOTN <EBOTN@mail.ru>
2023-05-06 16:10:35 +0700

    add swagger to member progress dto

--- src/task/dto/member-progress-dto.ts
+++ src/task/dto/member-progress-dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class MemberProgressDto{
+    @ApiProperty({description: 'Task in progress?'})
     readonly isRunning: boolean;
+
+    @ApiProperty({description: 'How much time are tracked'})
     readonly trackedTime: number;
+
+    @ApiProperty({description: 'osas'})
     readonly lastTimeLineStartTime: Date
 }
