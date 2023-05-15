c0cbcf9935af5d5e17e18543c72ee6ffb27a35e8
EBOTN <EBOTN@mail.ru>
2023-05-15 20:42:31 +0700

    change to front_url from env

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { MailerService } from "@nestjs-modules/mailer";
+import { env } from "process";
 
 @Injectable()
 export class EmailService {
   constructor(private mailerService: MailerService) {}
 
   async sendPasswordRecovery(email: string, firstName: string, token: string) {
-    const url = `http://localhost:5173/recovery-password?token=${token}`;
+    const url = `${env.FRONT_URL}/recovery-password?token=${token}`;
     return await this.mailerService.sendMail({
       to: email,
       subject: 'Tempus password reset request',
       template: "./changePasswordTemplate",
       context: {
         title: 'test',
-        siteUrl: "http://localhost:5173",
+        siteUrl: env.FRONT_URL,
         firstName,
         url,
       },
@@ -21,7 +22,7 @@ export class EmailService {
   }
 
   async sendChangeMail(email: string, token: string) {
-    const url = `http://localhost:5173/api/user/confirmChangeMail/${token}`;
+    const url = `${env.FRONT_URL}/user/confirmChangeMail/${token}`;
     return await this.mailerService.sendMail({
       to: email,
       template: "./bebra",
