a5ba89073d6d99b73779f37a6a16906600fdcfac
EBOTN <EBOTN@mail.ru>
2023-06-13 19:46:33 +0700

    some fix

--- src/task/dto/validation-user-id-dto.ts
+++ src/task/dto/validation-user-id-dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNumber } from "class-validator";
 
 export class ValidationUserIdDto{
     @ApiProperty({description: "User id", example: 1})
     @IsNotEmpty()
+    @IsNumber()
     @Type(() => Number)
     readonly userId: number
 }
